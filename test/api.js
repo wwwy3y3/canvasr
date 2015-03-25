07298e846017c486cd79fd5ced2c18c2e6f6ce3a
william <wwwy3y3@gmail.com>
2015-03-25 21:35:35 +0800

    done tests

--- test/api.js
+++ test/api.js
@@ -4,46 +4,56 @@ var should= require('should');
 var sizeOf = require('image-size');
 var testPng= __dirname+'/himym.png';
 
+describe('api', function () {
+   it('should resize and save', function (done) {
+      var cvr= new Canvasr();
+      cvr.readFile(testPng)
+         .resize({
+               width: 200,
+               height: 200
+         })
+         .then(function (cvr) {
+               return cvr.writePng(__dirname+'/himym-200.png');
+         })
+         .done(function () {
+               var dimensions = sizeOf(__dirname+'/himym-200.png');
+               var originalDimensions= sizeOf(__dirname+'/himym.png');
+               var origianlRatio= Math.round((originalDimensions.height/originalDimensions.width)*100)/100;
+               var ratio= dimensions.height/dimensions.width;
+               origianlRatio.should.be.equal(ratio);
+               done();
+               //console.log(fs.statSync(__dirname+'/himym-200.png'))
+         }, function (err) {
+               throw err;
+         })
+   })
 
-var cvr= new Canvasr();
-cvr.readFile(testPng)
-   .resize({
-   		width: 200,
-   		height: 200
+   it('should crop and save', function (done) {
+      var cvr= new Canvasr();
+      cvr.readFile(testPng)
+         .crop({
+               x: 100,
+               y: 100,
+               width: 200,
+               height: 200
+         })
+         .then(function (cvr) {
+               return cvr.writePng(__dirname+'/himym-cropped.png');
+         })
+         .done(function () {
+               var dimensions = sizeOf(__dirname+'/himym-cropped.png');
+               dimensions.width.should.be.equal(200);
+               dimensions.height.should.be.equal(200);
+               done();
+               //console.log(fs.statSync(__dirname+'/himym-cropped.png'))
+         }, function (err) {
+               throw err;
+         })
    })
-   .then(function (cvr) {
-   		return cvr.writePng(__dirname+'/himym-200.png');
-   })
-   .done(function () {
-   		//var dimensions = sizeOf(__dirname+'/himym-200.png');
-   		//var originalDimensions= sizeOf(__dirname+'/himym.png');
-   		//var origianlRatio= originalDimensions.height/originalDimensions.width;
-   		//var ratio= dimensions.height/dimensions.width;
-   		//origianlRatio.should.be.equal(ratio);
-   		console.log(fs.statSync(__dirname+'/himym-200.png'))
-   }, function (err) {
-   		throw err;
-   })
+})
+
 		 
 
-var cvr= new Canvasr();
-cvr.readFile(testPng)
-   .crop({
-   		x: 100,
-   		y: 100,
-   		width: 200,
-   		height: 200
-   })
-   .then(function (cvr) {
-   		return cvr.writePng(__dirname+'/himym-cropped.png');
-   })
-   .done(function () {
-   		//var dimensions = sizeOf(__dirname+'/himym-cropped.png');
-   		//dimensions.width.should.be.equal(200);
-   		//dimensions.height.should.be.equal(200);
-   		console.log(fs.statSync(__dirname+'/himym-cropped.png'))
-   }, function (err) {
-   		throw err;
-   })
+
 
 
